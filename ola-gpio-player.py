156af1c67b58a2a99d474ca880dc2cf36ac2950b
Martin Strohmeier <59908941+m0dbot@users.noreply.github.com>
2020-01-16 22:50:32 +0100

    Update ola-gpio-player.py
    tanslate comments, cleanup

--- ola-gpio-player.py
+++ ola-gpio-player.py
@@ -4,67 +4,59 @@
 import time
 import RPi.GPIO as GPIO
 import os
- 
-# Zählweise der Pins festlegen
+
+# pin config for inputs and LEDs, must be the same number of variables! First one is freeze/blackout
 GPIO.setmode(GPIO.BOARD)
-
-# Pinkonfiguration für Inputs und LEDs, muss die gleiche Anzahl sein!
-inp = [37, 35, 31, 23, 21, 7, 3]
+inp = [37, 35, 31, 23, 21, 7, 3] 
 outp = [38, 36, 32, 24, 22, 5, 11]
-
 GPIO.setwarnings(False) 
 
-# Pins als Eingang/Ausgang festlegen
+# set pins as in/output
 j = 0
 while j < len(inp):
     GPIO.setup(inp[j], GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.setup(outp[j], GPIO.OUT)
     j = j + 1
 
-# Startsequenz 1x abefeuern
-os.system ('ola_recorder -p startseqneu -u 1 -i 1')
+# run startseqence one time
+os.system ('ola_recorder -p startseqence -u 1 -i 1')
 
 
-#i = 0
+#i = 0   # debug
 
 def doIfHigh(channel):
-    # Zugriff auf Variable i ermöglichen
-	global i
+#	global i   # debug
 	global inp
 	global outp
-# Wenn Eingang HIGH ist, Ausgabe im Terminal erzeugen
-	print "Eingang " + str(channel) + " HIGH " + str(i)
-# Recorder stoppen
+#	print "Eingang " + str(channel) + " HIGH " + str(i)    # debug
+# stop recorder
 	os.system ('killall -9 ola_recorder')
-# LEDs aus
+# all LEDs off
 	m = 0
 	while m < len (outp):
 		GPIO.output(outp[m], GPIO.LOW)
 		m = m + 1
-#Recorder wieder starten
+# LED on		
+	GPIO.output(outp[channel], GPIO.HIGH)
+# start recorder again
 	if channel == inp[1]:
-		os.system ('ola_recorder -p pastellcolors -u 1 -i 0 &')
-		GPIO.output(outp[1], GPIO.HIGH)
+		os.system ('ola_recorder -p pastellcolors -u 1 -i 0 &') # replace 'pastellcolors' with your recording's file name, located in /home/pi
 	elif channel == inp[2]:
 		os.system ('ola_recorder -p primarycolors2 -u 1 -i 0 &')
-		GPIO.output(outp[2], GPIO.HIGH)
 	elif channel == inp[3]:
 		os.system ('ola_recorder -p wwcw -u 1 -i 0 &')
-		GPIO.output(outp[3], GPIO.HIGH)
 	elif channel == inp[4]:
 		os.system ('ola_recorder -p softshow -u 1 -i 0 &')
-		GPIO.output(outp[4], GPIO.HIGH)
 	elif channel == inp[5]:
 		os.system ('ola_recorder -p hardshow -u 1 -i 0 &')
-		GPIO.output(outp[5], GPIO.HIGH)
 	elif channel == inp[6]:
 		os.system ('ola_recorder -p testshow2 -u 1 -i 0 &')
-		GPIO.output(outp[6], GPIO.HIGH)
-	elif channel == inp[0]:
-		GPIO.output(outp[0], GPIO.HIGH)   
-#	i = i + 1
+#	elif channel == inp[0]: # freeze button. For blackout, uncomment this line and next and create a 'black' file with ola_recorder
+#		os.system ('ola_recorder -p black -u 1 -i 0 &') 
+
+#	i = i + 1   # debug
      
-# Ereignis deklarieren
+# declare event
 k = 0
 while k < len (outp):
 	GPIO.add_event_detect(inp[k], GPIO.FALLING, callback = doIfHigh, bouncetime = 200)
